5ef213df1f2cb28f1be8b432aff2685cf58fb850
Matt Barkman <mb6306@att.com>
2018-07-22 17:33:03 -0400

    Added Game Clock and Move Count Functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 /*
  * Create a list that holds all of your cards
+This list holds all the card types defined in the HTML
  */
+const cardsAll = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o"
+"fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube",
+"fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle",
+"fa fa-bomb", "fa fa-bomb"];
+
+
+
 
 
 /*
@@ -36,3 +44,65 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+
+
+
+/* count moves */
+const totalMoves = 0;
+const moveCount = document.querySelector('.moves');
+const levelStars = document.querySelector('.stars');
+
+function moves () {
+  totalMoves++;
+  moveCount. innerHTML = totalMoves;
+
+  /*star ratings
+  min number of moves = 8 */
+
+  if (totalMoves > 10  && moves <= 20) {
+      document.getElementById('SuperStar').style.color = "white";
+  } else if (totalMoves <= 30) {
+    document.getElementById('champ').style.color = "white"
+  }
+
+};
+
+
+
+
+
+
+
+
+
+
+/*set up for game clock*/
+const gameClock = document.querySelector('.clock');
+let minutes = document.getElementById('mins');
+let seconds = document.getElementById('secs');
+let sixtySeconds = 0;
+
+/* start clock */
+/*info on setInterval method found at
+https://www.w3schools.com/jsref/met_win_setinterval.asp */
+
+function startClock() {
+    elapsedTime = setInterval(function() {
+      seconds.innerText++
+      if (seconds.innerText == 60) {
+        minutes.innerText++;
+        seconds.innerText = 0;
+      }
+    }, 1000);
+}
+
+/*info on clearInterval method found at
+https://www.w3schools.com/jsref/met_win_clearinterval.asp*/
+function restartGameClock() {
+  clearInterval(gameCLock);
+}
+
+document.querySelector(".restart").addEventListener('click', restartGameClock);
